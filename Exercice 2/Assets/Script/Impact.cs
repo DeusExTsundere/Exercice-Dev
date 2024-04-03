a82a1fd56303309b8210d2966d315178381830fc
DeusExTsundere <kiady2227@gmail.com>
2024-04-03 20:21:22 +0200

    Code Move

--- Exercice 2/Assets/Script/Impact.cs
+++ Exercice 2/Assets/Script/Impact.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
-public class Collision : MonoBehaviour
+public class Impact : MonoBehaviour
 {
     [SerializeField] private GameObject character;
+    [SerializeField] private int speed = 10;
+    public int vitesse { get { return speed; } }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == ("Obstacle"))
         {
             Destroy(character);
+            speed = 0;
         }
     }
 }
