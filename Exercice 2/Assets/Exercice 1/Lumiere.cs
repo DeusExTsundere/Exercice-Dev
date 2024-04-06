b0df7ef0416b595434dfb3e17404aa7ccf993da2
DeusExTsundere <kiady2227@gmail.com>
2024-04-06 20:07:15 +0200

    Exo 1 plus suite

--- Exercice 2/Assets/Exercice 1/Lumiere.cs
+++ Exercice 2/Assets/Exercice 1/Lumiere.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class Lumiere : MonoBehaviour
 {
     public bool active = true;
-    [SerializeField] private Color Couleur;
-    private Light _light;
+    private Color Couleur;
+    [SerializeField, Range(0,1)] public float Red;
+    [SerializeField, Range(0,1)] public float Green;
+    [SerializeField, Range(0,1)] public float Blue;
+    [SerializeField]private GameObject _light;
+    private Light lumiere;
 
     private void Start()
     {
-        _light = GetComponent<Light>();
+        lumiere = _light.GetComponent<Light>();
     }
     public void Interrupteur()
     {
         if (active == true)
         {
-            _light.intensity = 1;
+            lumiere.intensity = 1;
             active = false;
         }
         else if (active == false)
         {
-            _light.intensity = 0;
+            lumiere.intensity = 0;
             active = true;
         }
     }
 
     private void Update()
     {
-        _light.color = Color.Lerp( _light.color, Couleur , Mathf.PingPong(Time.time,1 ));
+        Couleur = new Color(Red, Green, Blue);
+        lumiere.color = Color.Lerp( lumiere.color, Couleur , Mathf.PingPong(Time.time,1 ));
     }
 
 }
